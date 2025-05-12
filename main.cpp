fefdf23ebc1b6f2eb519cd406259e439e17a1029
eogus <kddhh2021@gmail.com>
2025-05-12 15:10:09 +0900

    main.cpp code changed

--- main.cpp
+++ main.cpp
@@ -5,24 +5,13 @@ using namespace std;
 int main(void)
 {
     Mat src = imread("lenna.bmp", IMREAD_GRAYSCALE);
-    if (src.empty()) {
-        cerr << "Image load failed!" << endl;
-        return -1;
-     }
-    float alpha = 1.f;
-    Mat dst = Mat(src.rows, src.cols, src.type());
-
-    uchar src_pixel;
-    for (int row = 0; row < src.rows; row++) {
-        for (int col = 0; col < src.cols; col++) {
-            src_pixel = src.at<uchar>(row, col);
-            dst.at<uchar>(row, col)
-                = saturate_cast<uchar>(src_pixel + (src_pixel - 128) * alpha);
-        }
-    }
-
+    if (src.empty()) { cerr << "Image load failed!" << endl; return -1;}
+    float data[ ] = { -1, -1, 0, -1, 0, 1, 0, 1, 1 };
+    Mat emboss(3, 3, CV_32FC1, data);
+    Mat dst;
+    filter2D(src, dst, -1, emboss, Point(-1, -1), 128);
     imshow("src", src);
     imshow("dst", dst);
     waitKey();
-return 0;
+    return 0;
 }
