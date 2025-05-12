c392516d19f7d646a9827ff623ede2fc03929787
eogus <kddhh2021@gmail.com>
2025-05-12 15:28:54 +0900

    quiz 1

--- main.cpp
+++ main.cpp
@@ -4,14 +4,24 @@ using namespace cv;
 using namespace std;
 int main(void)
 {
-    Mat src = imread("lenna.bmp", IMREAD_GRAYSCALE);
+    Mat src = imread("rose.bmp", IMREAD_GRAYSCALE);
     if (src.empty()) { cerr << "Image load failed!" << endl; return -1;}
-    float data[ ] = { -1, -1, 0, -1, 0, 1, 0, 1, 1 };
+
+    float data[] = {
+        1/9.0, 1/9.0, 1/9.0,
+        1/9.0, 1/9.0, 1/9.0,
+        1/9.0, 1/9.0, 1/9.0
+    Í};
+
     Mat emboss(3, 3, CV_32FC1, data);
     Mat dst;
-    filter2D(src, dst, -1, emboss, Point(-1, -1), 128);
+
+    filter2D(src, dst, -1, emboss, Point(-1, -1), 0);
+
     imshow("src", src);
     imshow("dst", dst);
+
     waitKey();
+
     return 0;
 }
