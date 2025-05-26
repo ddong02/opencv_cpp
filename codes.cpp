ba10a17342cb611b3290e6f08ee00736ccc93d46
eogus <kddhh2021@gmail.com>
2025-05-26 11:42:36 +0900

    test commit

--- codes.cpp
+++ codes.cpp
@@ -1,27 +1,27 @@
 // 2025-05-20
-// chap 8-1
+// chap 8-2
 
-// 01
+// 1
 
 // #include <opencv2/opencv.hpp>
-// #include <iostream>
-
 // using namespace cv;
 // using namespace std;
-
 // int main(void)
 // {
 //     Mat src = imread("images/lenna.bmp");
-//     if (src.empty()) { cerr << "Image load failed!" << endl; return -1;}
+//     if (src.empty()) {cerr << "Image load failed!" << endl; return -1;}
+    
 //     Point2f srcPts[3], dstPts[3];
 //     srcPts[0] = Point2f(0, 0);
 //     srcPts[1] = Point2f(src.cols - 1, 0);
 //     srcPts[2] = Point2f(src.cols - 1, src.rows - 1);
-//     dstPts[0] = Point2f(0, 0);
-//     dstPts[1] = Point2f(src.cols / 2, 0);
-//     dstPts[2] = Point2f(src.cols / 2, src.rows / 2);
+//     dstPts[0] = Point2f(150, 100);
+//     dstPts[1] = Point2f(src.cols + 150 - 1, 100);
+//     dstPts[2] = Point2f(src.cols + 150 - 1, src.rows + 100 - 1);
+
 //     Mat M = getAffineTransform(srcPts, dstPts);
 //     cout << M << endl;
+
 //     Mat dst;
 //     warpAffine(src, dst, M, Size());
 //     imshow("src", src);
@@ -30,65 +30,16 @@
 //     return 0;
 // }
 
-// 2025-05-20
-// chap 8-1
-
-// 02
+// 4
 
 // #include <opencv2/opencv.hpp>
-// #include <iostream>
-
 // using namespace cv;
 // using namespace std;
-
 // int main(void)
 // {
 //     Mat src = imread("images/lenna.bmp");
-//     if (src.empty()) { cerr << "Image load failed!" << endl; return -1;}
-
-//     Point2f srcPts[3], dstPts[3];
-//     srcPts[0] = Point2f(0, 0);
-//     srcPts[1] = Point2f(src.cols - 1, 0);
-//     srcPts[2] = Point2f(src.cols - 1, src.rows - 1);
-//     dstPts[0] = Point2f(0, 0);
-//     dstPts[1] = Point2f(src.cols / 2, 0);
-//     dstPts[2] = Point2f(src.cols / 2, src.rows / 2);
-
-//     Mat M = getAffineTransform(srcPts, dstPts);
-
-//     Mat dst;
-//     warpAffine(src, dst, M, Size());
-
-//     vector<Point2f> src_center = {Point2f(src.cols / 2, src.rows / 2)};
-//     vector<Point2f> dst_center;
-//     transform(src_center, dst_center, M);
+//     if (src.empty()) {cerr << "Image load failed!" << endl; return -1;}
     
-//     cout << "src center : " << src_center[0] << endl;
-//     cout << "dst center : " << dst_center[0] << endl;
-
-//     imshow("src", src);
-//     imshow("dst", dst);
-//     waitKey();
-
-//     return 0;
-// }
-
-// 2025-05-20
-// chap 8-1
-
-// 03
-
-// #include <opencv2/opencv.hpp>
-// #include <iostream>
-
-// using namespace cv;
-// using namespace std;
-
-// int main(void)
-// {
-//     Mat src = imread("images/lenna.bmp");
-//     if (src.empty()) { cerr << "Image load failed!" << endl; return -1;}
-
 //     Point2f srcPts[3], dstPts[3];
 //     srcPts[0] = Point2f(0, 0);
 //     srcPts[1] = Point2f(src.cols - 1, 0);
@@ -108,80 +59,31 @@
 
 //     imshow("src", src);
 //     imshow("dst", dst);
-//     waitKey();
 
+//     waitKey();
 //     return 0;
 // }
 
-// 2025-05-20
-// chap 8-1
-
-// 04
+// 5
 
 // #include <opencv2/opencv.hpp>
-// #include <iostream>
-
 // using namespace cv;
 // using namespace std;
-
 // int main(void)
 // {
-//     VideoCapture cap(1);
-//     if (!cap.isOpened()) { cerr << "Camera open failed!" << endl; return -1; }
+//     Mat src = imread("images/lenna.bmp");
+//     if (src.empty()) {cerr << "Image load failed!" << endl; return -1;}
+//     Mat dst;
+    
+//     Mat M = Mat_<double>({ 2, 3 }, { 0.5, 0, 0, 0, 0.5, 0 }); 
+//     warpAffine(src, dst, M, Size());
 
-//     Mat frame;
-//     int key = -1;
+//     M = Mat_<double>({ 2, 3 }, { 1, 0, src.cols * 0.25 - 1, 0, 1, src.rows * 0.25 - 1 });
+//     warpAffine(dst, dst, M, Size());
 
-//     cap >> frame;
+//     imshow("src", src);
+//     imshow("dst2", dst);
 
-//     Point2f orgPts[3];
-//     orgPts[0] = Point2f(0, 0);
-//     orgPts[1] = Point2f(frame.cols - 1, 0);
-//     orgPts[2] = Point2f(frame.cols - 1, frame.rows - 1);
-
-//     Point2f zoominPts[3];
-//     zoominPts[0] = Point2f(0, 0);
-//     zoominPts[1] = Point2f(frame.cols * 2 - 1, 0);
-//     zoominPts[2] = Point2f(frame.cols * 2 - 1, frame.rows * 2 - 1);
-
-//     Point2f zoomoutPts[3];
-//     zoomoutPts[0] = Point2f(0, 0);
-//     zoomoutPts[1] = Point2f(frame.cols / 2 - 1, 0);
-//     zoomoutPts[2] = Point2f(frame.cols / 2 - 1, frame.rows / 2 - 1);
-
-//     Mat M;
-
-//     bool zoomout = false;
-//     bool zoomin = false;
-
-//     while (true) {
-//         cap >> frame;
-        
-//         if (zoomout) {            
-//             M = getAffineTransform(orgPts, zoomoutPts);
-//             warpAffine(frame, frame, M, Size());
-//         }
-//         else if (zoomin) {
-//             M = getAffineTransform(orgPts, zoominPts);
-//             warpAffine(frame, frame, M, Size());
-//         }
-//         imshow("camera", frame);
-//         key = waitKey(5);
-
-//         if (key == 'q') break;
-//         if (key == 'a') {
-//             zoomout = true;
-//             zoomin = false;
-//         }
-//         if (key == 'b'){
-//             zoomin = true;
-//             zoomout = false;
-//         }
-//         if (key == 'c') {
-//             zoomin = false;
-//             zoomout = false;
-//         }
-//     }
-
+//     waitKey();
 //     return 0;
 // }
--- main.cpp
+++ main.cpp
@@ -1,8 +1,3 @@
-// 2025-05-20
-// chap 8-1
-
-// 0
-
 #include <opencv2/opencv.hpp>
 #include <iostream>
 
@@ -11,7 +6,44 @@ using namespace std;
 
 int main(void)
 {
-    
+    Mat src = imread("images/lenna.bmp");
+    if (src.empty()) {
+        cerr << "Image load failed!" << endl;
+        return -1;
+    }
+
+    imshow("src", src);
+    Mat dst;
+
+    int flipCode[] = { 1, 0, -1 };
+    for (int i = 0; i < 3; i++) {
+        Mat affine;
+
+        int w = src.cols;
+        int h = src.rows;
+
+        if (flipCode[i] == 1) {
+            affine = (Mat_<double>(2, 3) << -1, 0, w - 1,
+                                             0, 1, 0);
+        }
+        else if (flipCode[i] == 0) {
+            affine = (Mat_<double>(2, 3) << 1, 0, 0,
+                                            0, -1, h - 1);
+        }
+        else if (flipCode[i] == -1) {
+            affine = (Mat_<double>(2, 3) << -1, 0, w - 1,
+                                             0, -1, h - 1);
+        }
+
+        warpAffine(src, dst, affine, src.size(), INTER_LINEAR, BORDER_CONSTANT, Scalar(0,0,0));
+
+        String desc = format("flipCode: %d", flipCode[i]);
+        putText(dst, desc, Point(10, 30), FONT_HERSHEY_SIMPLEX, 1.0,
+                Scalar(255, 0, 0), 1, LINE_AA);
+
+        imshow("dst", dst);
+        waitKey();
+    }
 
     return 0;
 }
