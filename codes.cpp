794d78ec442cd8c0fd1b6fbb4a8fbe80cf7184ae
eogus <kddhh2021@gmail.com>
2025-05-28 12:58:08 +0900

    chapter 12-1

--- codes.cpp
+++ codes.cpp
@@ -1,161 +1,121 @@
-// 2025-05-26
-// chap 11-1
+// 2025-05-28
+// chap 12-1
 
-// 02
+// 01
 
-// #include <opencv2/opencv.hpp>
 // #include <iostream>
+// #include <opencv2/opencv.hpp>
 
 // using namespace std;
 // using namespace cv;
-
-// // 회색조 히스토그램 계산 함수
-// Mat calcGrayHist(const Mat& img)
-// {
-//     CV_Assert(img.type() == CV_8UC1);
-
-//     Mat hist;
-//     int channels[] = {0};
-//     int dims = 1;
-//     const int histSize[] = {256};
-//     float graylevel[] = {0, 256};
-//     const float* ranges[] = {graylevel};
-
-//     calcHist(&img, 1, channels, noArray(), hist, dims, histSize, ranges);
-//     return hist;
-// }
-
-// // 히스토그램 시각화 함수
-// Mat getGrayHistImage(const Mat& hist)
-// {
-//     CV_Assert(hist.type() == CV_32F);
-//     CV_Assert(hist.size() == Size(1, 256));
-
-//     double histMax;
-//     minMaxLoc(hist, 0, &histMax);
-
-//     Mat histImage(100, 256, CV_8UC1, Scalar(255));
-//     for (int i = 0; i < 256; i++) {
-//         float binVal = hist.at<float>(i, 0);
-//         int intensity = cvRound(binVal * 100 / histMax);
-//         line(histImage, Point(i, 100), Point(i, 100 - intensity), Scalar(0));
-//     }
-
-//     return histImage;
-// }
 
 // int main()
 // {
-//     // 1. 이미지 불러오기
-//     Mat src = imread("images/lenna.bmp", IMREAD_GRAYSCALE);
-//     if (src.empty()) {
-//         cerr << "Error: Image load failed!" << endl;
-//         return -1;
+//     Mat src = imread("images/labelex.png", IMREAD_GRAYSCALE);
+//     if (src.empty()) { cerr << "Image load failed!" << endl; return -1;}
+//     Mat bin;
+//     threshold(src, bin, 0, 255, THRESH_BINARY_INV | THRESH_OTSU);
+
+//     Mat labels, stats, centroids;
+//     int cnt = connectedComponentsWithStats(bin, labels, stats, centroids);
+
+//     Mat dst;
+//     cvtColor(src, dst, COLOR_GRAY2BGR);
+
+//     int object_sizes[4];
+
+//     for (int i = 1; i < cnt; i++) {
+//         int* p = stats.ptr<int>(i); //stats객체의 i행의 시작주소를 리턴
+//         object_sizes[i-1] = p[4];
 //     }
 
-//     // 2. 히스토그램 계산 및 시각화
-//     Mat hist = calcGrayHist(src);
-//     Mat histImage = getGrayHistImage(hist);
-//     imshow("Histogram", histImage);
+//     int max_size_index = 0;
+//     int min_size_index = 0;
 
-//     // 3. Otsu 알고리즘으로 자동 임계값 결정 (출력 따로 받기)
-//     Mat binary;
-//     double thresh_val = threshold(src, binary, 0, 255, THRESH_BINARY | THRESH_OTSU);
-//     cout << "자동 임계값 (Otsu): " << thresh_val << endl;
+//     for (int i=1; i<4; i++){
+//         if (object_sizes[i] < object_sizes[min_size_index])
+//             min_size_index = i;
+//         else if (object_sizes[i] > object_sizes[max_size_index])
+//             max_size_index = i;
+//     }
 
-//     // 4. 결과 출력
-//     imshow("Original Image", src);    // 원본 이미지 그대로
-//     imshow("Binary Image", binary);   // 이진화된 이미지
+//     cout << "면적이 최대인 객체의 레이블: " << max_size_index + 1 << endl;
+//     cout << "무게 중심(x, y): " << centroids.at<double>(max_size_index + 1, 0) 
+//         << ", " << centroids.at<double>(max_size_index + 1, 1) << endl;
+
+//     cout << "면적이 최소인 객체의 레이블: " << min_size_index + 1 << endl;
+//     cout << "무게 중심(x, y): " << centroids.at<double>(min_size_index + 1, 0) 
+//         << ", " << centroids.at<double>(min_size_index + 1, 1) << endl;
+
+//     int* ptr_min = stats.ptr<int>(min_size_index+1);
+//     int* ptr_max = stats.ptr<int>(max_size_index+1);
+
+//     rectangle(dst, Rect(ptr_min[0], ptr_min[1], ptr_min[2], ptr_min[3]), Scalar(255, 0, 0));
+//     rectangle(dst, Rect(ptr_max[0], ptr_max[1], ptr_max[2], ptr_max[3]), Scalar(0, 0, 255));
+
+//     imshow("src", src);
+//     imshow("dst", dst);
 
 //     waitKey(0);
 //     destroyAllWindows();
+
 //     return 0;
 // }
 
-// 03
+// 02
 
-
-
-
-// 04
-
-// #include <opencv2/opencv.hpp>
 // #include <iostream>
+// #include <opencv2/opencv.hpp>
 
 // using namespace std;
 // using namespace cv;
 
-// // 회색조 히스토그램 계산
-// Mat calcGrayHist(const Mat& img)
+// int main()
 // {
-//     CV_Assert(img.type() == CV_8UC1);
+//     Mat src = imread("images/polygon.bmp");
+//     if (src.empty()) { cerr << "Image load failed!" << endl; return -1;}
 
-//     Mat hist;
-//     int channels[] = {0};
-//     int dims = 1;
-//     const int histSize[] = {256};
-//     float graylevel[] = {0, 256};
-//     const float* ranges[] = {graylevel};
+//     Mat src_gray;
+//     cvtColor(src, src_gray, COLOR_BGR2GRAY);
 
-//     calcHist(&img, 1, channels, noArray(), hist, dims, histSize, ranges);
-//     return hist;
-// }
+//     Mat bin;
+//     threshold(src_gray, bin, 0, 255, THRESH_BINARY_INV | THRESH_OTSU);
 
-// // 히스토그램 시각화
-// Mat getGrayHistImage(const Mat& hist)
-// {
-//     CV_Assert(hist.type() == CV_32F);
-//     CV_Assert(hist.size() == Size(1, 256));
+//     Mat labels, stats, centroids;
+//     int cnt = connectedComponentsWithStats(bin, labels, stats, centroids);
 
-//     double histMax;
-//     minMaxLoc(hist, 0, &histMax);
+//     int* stats_ptr;
+//     double* centroids_ptr;
 
-//     Mat imgHist(100, 256, CV_8UC1, Scalar(255));
-//     for (int i = 0; i < 256; i++) {
-//         line(imgHist,
-//              Point(i, 100),
-//              Point(i, 100 - cvRound(hist.at<float>(i, 0) * 100 / histMax)),
-//              Scalar(0));
+//     for (int i=1; i<cnt; i++){
+//         stats_ptr = stats.ptr<int>(i);
+//         centroids_ptr = centroids.ptr<double>(i);
+
+//         cout << i << ' ';
+//             for (int j=0; j<5; j++)
+//             cout << stats_ptr[j] << ' ';
+//         cout << src.at<Vec3b>(centroids_ptr[1], centroids_ptr[0]) << endl;
+
+//         rectangle(src,
+//             Rect(stats_ptr[0], stats_ptr[1], stats_ptr[2], stats_ptr[3]),
+//             Scalar(0, 255, 255)
+//         );
+
+//         putText(
+//             src,
+//             format("%d", i),
+//             Point(centroids_ptr[0], centroids_ptr[1]),
+//             FONT_HERSHEY_SIMPLEX,
+//             1.0,
+//             Scalar(0,0,255)
+//         );
 //     }
 
-//     return imgHist;
-// }
+//     imshow("src", src);
+//     imshow("src_gray", src_gray);
 
-// int main(void)
-// {
-//     VideoCapture cap("images/line.mp4");
-//     if (!cap.isOpened()) {
-//         cerr << "Error: Cannot open the video file!" << endl;
-//         return -1;
-//     }
-
-//     Mat frame, gray, binary;
-
-//     while (true) {
-//         cap >> frame;
-//         if (frame.empty())
-//             break;
-
-//         cvtColor(frame, gray, COLOR_BGR2GRAY);
-
-//         // 히스토그램 계산 및 출력
-//         Mat hist = calcGrayHist(gray);
-//         Mat histImg = getGrayHistImage(hist);
-//         imshow("Histogram", histImg);
-
-//         // 자동 임계값 찾기 (Otsu)
-//         double thresh_val = threshold(gray, binary, 0, 255, THRESH_BINARY | THRESH_OTSU);
-
-//         // 이진 영상 출력
-//         imshow("Binary", binary);
-//         imshow("Frame", frame);
-//         cout << "Current Otsu Threshold: " << thresh_val << endl;
-
-//         char key = waitKey(30);
-//         if (key == 27 || key == 'q') break; // ESC or q to quit
-//     }
-
-//     cap.release();
+//     waitKey(0);
 //     destroyAllWindows();
+
 //     return 0;
 // }
--- main.cpp
+++ main.cpp
@@ -1,72 +1,55 @@
-#include <opencv2/opencv.hpp>
 #include <iostream>
+#include <opencv2/opencv.hpp>
 
 using namespace std;
 using namespace cv;
 
-Mat calcGrayHist(const Mat& img)
-{
-    CV_Assert(img.type() == CV_8UC1);
-
-    Mat hist;
-    int channels[] = {0};
-    int dims = 1;
-    const int histSize[] = {256};
-    float graylevel[] = {0, 256};
-    const float* ranges[] = {graylevel};
-
-    calcHist(&img, 1, channels, noArray(), hist, dims, histSize, ranges);
-    return hist;
-}
-
-Mat getGrayHistImage(const Mat& hist)
-{
-    CV_Assert(hist.type() == CV_32F);
-    CV_Assert(hist.size() == Size(1, 256));
-
-    double histMax;
-    minMaxLoc(hist, 0, &histMax);
-
-    Mat histImage(100, 256, CV_8UC1, Scalar(255));
-    for (int i = 0; i < 256; i++) {
-        float binVal = hist.at<float>(i, 0);
-        int intensity = cvRound(binVal * 100 / histMax);
-        line(histImage, Point(i, 100), Point(i, 100 - intensity), Scalar(0));
-    }
-
-    return histImage;
-}
-
 int main()
 {
-    // 1. 이미지 불러오기
-    Mat src = imread("images/lenna.bmp", IMREAD_GRAYSCALE);
-    if (src.empty()) {
-        cerr << "Error: Image load failed!" << endl;
-        return -1;
+    Mat src = imread("images/polygon.bmp");
+    if (src.empty()) { cerr << "Image load failed!" << endl; return -1;}
+
+    Mat src_gray;
+    cvtColor(src, src_gray, COLOR_BGR2GRAY);
+
+    Mat bin;
+    threshold(src_gray, bin, 0, 255, THRESH_BINARY_INV | THRESH_OTSU);
+
+    Mat labels, stats, centroids;
+    int cnt = connectedComponentsWithStats(bin, labels, stats, centroids);
+
+    int* stats_ptr;
+    double* centroids_ptr;
+
+    for (int i=1; i<cnt; i++){
+        stats_ptr = stats.ptr<int>(i);
+        centroids_ptr = centroids.ptr<double>(i);
+
+        cout << i << ' ';
+            for (int j=0; j<5; j++)
+            cout << stats_ptr[j] << ' ';
+        cout << src.at<Vec3b>(centroids_ptr[1], centroids_ptr[0]) << endl;
+
+        rectangle(src,
+            Rect(stats_ptr[0], stats_ptr[1], stats_ptr[2], stats_ptr[3]),
+            Scalar(0, 255, 255)
+        );
+
+        putText(
+            src,
+            format("%d", i),
+            Point(centroids_ptr[0], centroids_ptr[1]),
+            FONT_HERSHEY_SIMPLEX,
+            1.0,
+            Scalar(0,0,255)
+        );
     }
 
-    // 2. 히스토그램 계산 및 시각화
-    Mat hist = calcGrayHist(src);
-    Mat histImage = getGrayHistImage(hist);
-    imshow("Histogram", histImage);
-
-    // 3. 적응형 이진화
-    Mat adaptive_binary;
-    adaptiveThreshold(
-        src, adaptive_binary,
-        255,                       // 최대값
-        ADAPTIVE_THRESH_MEAN_C,   // 또는 ADAPTIVE_THRESH_GAUSSIAN_C
-        THRESH_BINARY,
-        11,  // blockSize (이웃 영역 크기, 홀수)
-        5    // C 값 (평균에서 빼는 값, 조정용)
-    );
-
-    // 4. 결과 출력
-    imshow("Original Image", src);
-    imshow("Adaptive Binary", adaptive_binary);
+    imshow("src", src);
+    imshow("src_gray", src_gray);
 
     waitKey(0);
     destroyAllWindows();
+
     return 0;
 }
